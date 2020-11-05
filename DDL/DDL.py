904e2943630236ba731bb159832cbb56026def98
leejaehoon2016 <ljw5694@naver.com>
2020-11-05 10:21:50 +0900

    Update DDL.py
    DDL version2

--- DDL/DDL.py
+++ DDL/DDL.py
@@ -1,5 +1,5 @@
 import mysql.connector
-
+# 마리아db설치하시고 -> create database db_name -> connect 메소드 부분에 패스워드랑 데이터베이스 이름만 바꿔서 사용
 # mysql connection을 선언한다. 파라미터는 host는 접속 주소, user는 ID, passwd는 패스워드, database는 접속할 데이터 베이스이다.	
 dbconn = mysql.connector.connect(host="localhost", user="root", passwd="2657", database="testdb")
 cs = dbconn.cursor()
@@ -16,7 +16,7 @@ Address      TEXT               NOT NULL,
 DateOfBirth  DATE               NOT NULL,
 PhoneNumber  VARCHAR(11)        NOT NULL,
 CONSTRAINT domain_sex CHECK (Gender='M' OR Gender="F"),
-CONSTRAINT domain_user_flag CHECK (ID='ad_%' OR ID='as_%' OR ID='su_%'),
+CONSTRAINT domain_user_flag CHECK (ID LIKE 'ad %' OR ID LIKE 'as %' OR ID LIKE 'su %'),
 PRIMARY KEY(ID)
 );
 """)
@@ -30,7 +30,7 @@ TableName            VARCHAR(40)        NOT NULL,
 TaskSchema           TEXT               NOT NULL,
 Name                 VARCHAR(40)        NOT NULL,
 Description          MEDIUMTEXT         NOT NULL,
-CONSTRAINT domain_task_threshold CHECK (TaskThreshold >=0 and TaskThreshold <= 100),
+CONSTRAINT domain_task_threshold CHECK (TaskThreshold >=0 and TaskThreshold <= 10),
 PRIMARY KEY(TaskID),
 UNIQUE(TableName)
 );
@@ -69,8 +69,8 @@ PRIMARY KEY(SubmissionID),
 CONSTRAINT domain_QuanAssessment CHECK (QuanAssessment >=0 and QuanAssessment <= 10),
 CONSTRAINT domain_QualAssessment CHECK ((QualAssessment >=0 and QualAssessment <= 10) OR QualAssessment IS NULL),
 CONSTRAINT domain_P_NP CHECK (P_NP = 'NP' OR P_NP = 'P'),
-CONSTRAINT domain_SubmitterID CHECK (SubmitterID='su_%'),
-CONSTRAINT domain_AssessorID CHECK (AssessorID = 'as_%')
+CONSTRAINT domain_SubmitterID CHECK (SubmitterID LIKE 'su %'),
+CONSTRAINT domain_AssessorID CHECK (AssessorID LIKE 'as %')
 );
 """)
 
