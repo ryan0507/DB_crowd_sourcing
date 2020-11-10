f7028aaf4be473bdc0b4462d9199b6249e1a9a97
hanchaeeun <yaebo98@naver.com>
2020-11-10 14:14:32 +0900

    feat: add admin_taskAdd, admin_taskInfo

--- catalog/templates/catalog/my-app/src/App.tsx
+++ catalog/templates/catalog/my-app/src/App.tsx
@@ -8,6 +8,8 @@ import {
   NavLink,
 } from 'react-router-dom';
 import admin_main from "./pages/admin_main";
+import admin_taskAdd from "./pages/admin_taskAdd";
+import admin_taskInfo from "./pages/admin_taskInfo";
 
 function App() {
   return (
@@ -24,8 +26,10 @@ function App() {
                     </div>
                 </div>
                 <Switch>
-                    <Route exact={true} path="/" component={admin_main}/>
+                    <Route exact path="/" component={admin_main}/>
                     <Route exact={true} path="/admin_main" component={admin_main}/>
+                    <Route exact path="/admin_taskAdd" component={admin_taskAdd}/>
+                    <Route exact path="/admin_taskInfo" component={admin_taskInfo}/>
                 </Switch>
             </body>
         </div>
--- catalog/templates/catalog/my-app/src/pages/admin_main.tsx
+++ catalog/templates/catalog/my-app/src/pages/admin_main.tsx
@@ -1,10 +1,11 @@
+import { BrowserRouter as Router, Route,Link } from 'react-router-dom';
 import React from 'react';
 
 function admin_main(){
    return(
        <div className="wrapper">
            <div className="Title">태스크 목록</div>
-           <div className="right_side_small">태스크 추가하기</div>
+           <Link to = "./admin_taskAdd" className = "right_side_small">태스크 추가하기</Link>
            <div className="content_list"> 내용 </div>
        </div>
    );
