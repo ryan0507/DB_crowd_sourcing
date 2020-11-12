bd21956188a390cb48431f0ea15566c1eb2a2b32
hanchaeeun <yaebo98@naver.com>
2020-11-12 21:49:16 +0900

    feat: add login

--- catalog/templates/catalog/my-app/src/App.tsx
+++ catalog/templates/catalog/my-app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
         <div className="App">
             <body>
-                {admin()}
+                {login()}
             </body>
         </div>
   );
--- catalog/templates/catalog/my-app/src/pages/login.tsx
+++ catalog/templates/catalog/my-app/src/pages/login.tsx
@@ -3,7 +3,18 @@ import { BrowserRouter as Router, Route, Link, Switch, NavLink, } from 'react-ro
 
 function login() {
   return (
-      <div></div>
+    <div className="wrapper">
+      <div className="formContent_signin">
+        <h2 className="active"> Sign In </h2>
+        <h2 className="inactive underlineHover">Sign Up </h2>
+
+        <div className="sign">
+            <input type="text" id="login" name="login" placeholder="login"></input>
+            <input type="text" id="password" name="login" placeholder="password"></input>
+            <input type="submit" value="Log In"></input>
+        </div>
+      </div>
+    </div>
   );
 }
 
