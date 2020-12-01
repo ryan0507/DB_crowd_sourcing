d25331f647c59533ef8e09c411006beafe84a27b
2suh <leesuh_7@daum.net>
2020-12-01 09:27:59 +0900

    submitterView, add

--- adminUI/views.py
+++ adminUI/views.py
@@ -61,16 +61,19 @@ def TaskInfoView(request, infoID):
 
     info_lst = []
 
-    sql = """SELECT T.TaskID, T.Name, T.Description
-             FROM TASK T
-             WHERE T.TaskID = %s"""
+    sql = """SELECT T.TaskID, T.Name, T.TaskThreshold, O.Mapping, R.Name, 
+                P.P_NP, D.QualAssessment, O.OriginSchema, S.Name, D.FileName, D.P_NP
+             FROM TASK T, ORIGINAL_DATA_TYPE O, PARSING_DATA D, PARTICIPATE_TASK P, USER R, USER S
+             WHERE T.TaskID = %s AND T.TaskID = O.TaskID AND T.TaskID = P.TaskID AND O.OriginalTypeID = D.OriginalTypeID
+                AND R.MainID = P.SubmitterID AND S.MainID = D.SubmitterID"""
 
     list_arg = []
     list_arg.append(infoID)
 
     for info in selectDetail(sql, list_arg):
-        info_dict = {"TaskID": info[0], "Name": info[1], "Description": info[2]}
-        #print(info_dict)
+        info_dict = {"TaskID": info[0], "Name": info[1], "Threshold": info[2], "Mapping": info[3],
+                     "RequestName": info[4], "Request": info[5], "QualAssessment": info[6], "OriginSchema": info[7],
+                     "SubmitterName": info[8], "FileName": info[9], "P_NP": info[10]}
         info_lst.append(info_dict)
 
     return JsonResponse(info_dict, safe=False)
@@ -79,17 +82,17 @@ def FileDetailView(request, infoID, fileID):
 
     file_lst = []
 
-    sql = """SELECT OriginalTypeID, TaskID, OriginSchema
-          FROM ORIGINAL_DATA_TYPE WHERE TaskID = %s AND OriginalTypeID = %s"""
+    sql = """SELECT O.OriginalTypeID, O.TaskID, O.OriginSchema, O.Mapping, P.FileName
+          FROM ORIGINAL_DATA_TYPE O, PARSING_DATA P 
+          WHERE O.OriginalTypeID = P.OriginalTypeID AND O.TaskID = %s AND O.OriginalTypeID = %s"""
 
     list_arg = []
     list_arg.append(infoID)
     list_arg.append(fileID)
-    #print("list_arg : ", list_arg)
 
     for file in selectDetail(sql, list_arg):
-        file_dict = {"OriginalTypeID": file[0], "TaskID": file[1], "OriginSchema": file[2]}
-        #print(file_dict)
+        file_dict = {"OriginalTypeID": file[0], "TaskID": file[1], "OriginSchema": file[2],
+                     "Mapping": file[3], "Files": file[4]}
         file_lst.append(file_dict)
 
     return JsonResponse(file_dict, safe=False)
@@ -100,13 +103,11 @@ def DataTypeAddView(request, infoID):
     if len(request.body) != 0:
         body_unicode = request.body.decode('utf-8')
         data = json.loads(body_unicode)
-        val_dict = {"OriginSchema": data["OriginSchema"], "Mapping": data["Mapping"]}
-        merge("INSERT INTO Origianl_Data VALUES (%s %s)", val_dict)
-        val_dict["TaskID"] = infoID
-        value_lst.append(val_dict)
+        val_tuple = (data["OriginSchema"],data["Mapping"])
+        merge("INSERT INTO Origianl_Data(OriginSchema, Mapping) VALUES (%s %s)", val_tuple)
+        value_lst.append(val_tuple)
         return JsonResponse(value_lst, safe=False)
     else:
-        #print("No data")
         return JsonResponse({})
 
 def TableSchemaAddView(request, infoID):
@@ -115,13 +116,11 @@ def TableSchemaAddView(request, infoID):
     if len(request.body) != 0:
         body_unicode = request.body.decode('utf-8')
         data = json.loads(body_unicode)
-        val_dict = {"TaskSchema": data["TaskSchema"]}
-        merge("INSERT INTO TASK VALUES (%s)", val_dict)
-        val_dict["TaskID"] = infoID
+        val_dict = (data["TaskSchema"])
+        merge("INSERT INTO TASK(TaskSchema) VALUES (%s)", val_dict)
         value_lst.append(val_dict)
         return JsonResponse(value_lst, safe=False)
     else:
-        #print("No data")
         return JsonResponse({})
 
 def UserListView(request):
@@ -165,11 +164,8 @@ def UserListView(request):
 
 def PresenterDetailView(request, su_ID):
 
-    task_lst = []
-    file_lst = []
-
-    sql = """SELECT A.ID, T.Name, P.SubmissionID, P.SubmissionDate, P.FileName, P.QuanAssessment, P.P_NP 
-                    FROM PARSING_DATA P, Task T, Original_Data_Type O, USER A
+    sql = """SELECT A.ID, T.Name, P.SubmissionDate, P.FileName, P.P_NP 
+                    FROM PARSING_DATA P, TASK T, ORIGINAL_DATA_TYPE O, USER A
                     WHERE O.OriginalTypeID = P.OriginalTypeID AND T.TaskID = O.TaskID 
                     AND A.MainID = P.SubmitterID AND P.SubmitterID = %s"""
 
@@ -177,11 +173,22 @@ def PresenterDetailView(request, su_ID):
     list_arg = []
     list_arg.append(su_ID)
 
+    pre_dict = {}
     for row in selectDetail(sql, list_arg):
-        pre_dict = {"ID" : row[0]}
-        pre_dict = {"SubmissionID": row[0], "SubmissionDate": row[1], "FileName": row[2],
-                    "QuanAssessment" : row[3], "P_NP": row[4]}
-        file_lst.append(pre_dict)
+        pre_dict["ID"] = row[0]
+        file_dict = {"TaskName": row[1], "SubmissionDate": row[2], "FileName": row[3], "P_NP": row[4]}
+        taskName = file_dict["TaskName"]
+        del(file_dict["TaskName"])
+        if taskName not in pre_dict.keys():
+            pre_dict[taskName] = {}
+            pre_dict[taskName]["Files"] = []
+            pre_dict[taskName]["Total"] = 1
+            pre_dict[taskName]["Pass"] = 0
+        else:
+            pre_dict[taskName]["Total"] += 1
+        if file_dict["P_NP"] == "P": pre_dict[taskName]["Pass"] += 1
+        pre_dict[taskName]["Files"].append(file_dict)
+
     return JsonResponse(pre_dict, safe=False)
 
 def EstimatorDetailView(request, as_ID):
@@ -200,7 +207,7 @@ def EstimatorDetailView(request, as_ID):
     pass_num = 0
     est_lst = []
     for row in selectDetail(sql, list_arg):
-        est_dict = {"ID": row[0]}
+        as_ID = row[0]
         file_dict = {"TaskName": row[1], "SubmitterName": row[2], "Filename": row[3],
                      "QualAssessment": row[4], "P_NP": row[5]}
         file_dict["QualAssessment"] = int(file_dict["QualAssessment"])
@@ -208,6 +215,7 @@ def EstimatorDetailView(request, as_ID):
         total_num += 1
         if row[5] != 'W': check_num += 1
         if row[5] == 'P': pass_num += 1
+    est_dict = {"ID": as_ID}
     est_dict["Files"] = est_lst
     est_dict["Total"] = total_num
     est_dict["Check"] = check_num
