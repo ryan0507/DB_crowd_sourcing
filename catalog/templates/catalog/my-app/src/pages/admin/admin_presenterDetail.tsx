bda8d3627b65ec19b8e29caad693a0cca2c27273
hanchaeeun <yaebo98@naver.com>
2020-12-01 12:06:23 +0900

    feat : edit admin

--- catalog/templates/catalog/my-app/src/pages/admin/admin_presenterDetail.tsx
+++ catalog/templates/catalog/my-app/src/pages/admin/admin_presenterDetail.tsx
@@ -15,16 +15,23 @@ axios.defaults.xsrfCookieName = "csrftoken";
 axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
 interface submitter {
-    // ID : string,
-    SubmissionID: number,
-    SubmissionDate: string,
-    StartDate: string,
-    FileName: string,
-    P_NP: string,
+    Tasks : task[],
+    ID : string,
+}
+interface task{
+    TaskName : string,
+    Files: file[],
+    Total : number,
+    Pass : number,
+}
+interface file{
+    SubmissionDate : string,
+    FileName : string,
+    P_NP : string,
 }
 
 interface Column {
-  id:  'date' | 'time' | 'fileName' | 'pNp';
+  id:  'SubmissionDate' | 'FileName' | 'P_NP';
   label: string;
   minWidth?: number;
   align?: 'center';
@@ -33,19 +40,14 @@ interface Column {
 }
 
 const columns: Column[] = [
-  { id: 'date', label: '제출일', minWidth: 80 },
+  { id: 'SubmissionDate', label: '제출일', minWidth: 80 },
   {
-    id: 'time',
-    label: '제출시간',
-    minWidth: 90,
-  },
-  {
-    id: 'fileName',
+    id: 'FileName',
     label: '제출\u00a0파일명',
     minWidth: 140,
   },
   {
-    id: 'pNp',
+    id: 'P_NP',
     label: 'Pass\u00a0여부',
     minWidth: 100,
   },
@@ -62,23 +64,37 @@ const useStyles = makeStyles({
 
 
 interface Data {
-  date: string;
-  time: string;
-  fileName: string;
-  pNp: string;
+    SubmissionDate : string,
+    FileName : string,
+    P_NP : string,
 }
 
-function createData(  date: string, time: string,fileName: string, pNp: string): Data {
-  return { date, time, fileName, pNp };
-}
+// function createData(  date: string, time: string,fileName: string, pNp: string): Data {
+//   return { date, time, fileName, pNp };
+// }
+// function createData(_file : file[]): file[] {
+//     let temp : file[] = [];
+//
+//     _file.map((item)=>{
+//
+//         let tempJoined : string[] = [];
+//         if(item.Task != []){
+//         item.Task?.map((joined)=>{
+//             if(joined != null) {tempJoined.push(joined+", ")}
+//             else {tempJoined.push('-')}
+//         })}
+//         temp.push({ID : item.ID, name: item.Name, birth: item.DateOfBirth, ages: item.age+"대", sex: item.Gender, role:item.role, joined: tempJoined, phoneNum: item.PhoneNumber,})
+//     })
+//   return temp;
+// }
 
-const rows = [
-  createData('20.11.01', '23 : 15 : 20', '음악은_즐거워.csv', 'Non-pass'),
-  createData( '20.11.02', '23 : 15 : 30', '새마을식당_10월.csv', 'Pass'),
-];
+// const rows = [
+//   createData('20.11.01', '23 : 15 : 20', '음악은_즐거워.csv', 'Non-pass'),
+//   createData( '20.11.02', '23 : 15 : 30', '새마을식당_10월.csv', 'Pass'),
+// ];
 
 export default function Admin_presenterDetail(props : RouteComponentProps<{su_ID : string}>,){
-    const [submitter, setSubmitter] = useState<submitter>({SubmissionID: 0, SubmissionDate: "", StartDate: "", FileName: "", P_NP: "",});
+    const [submitter, setSubmitter] = useState<submitter>({Tasks: [], ID : ''});
     const getApi = async() =>{
         await axios.get(`http://127.0.0.1:8000/adminUI/user/submitter/${props.match.params.su_ID}`).then((r)=>{
             let temp: submitter = r.data;
@@ -108,73 +124,77 @@ export default function Admin_presenterDetail(props : RouteComponentProps<{su_ID
    return(
        <div className={"presenterDetail"}>
        <div className="wrapper">
-           {submitter.FileName}
-           <div className="Title">{submitter.SubmissionID}</div>
+           <div className="Title">{submitter.ID}</div>
            <Link to = "/admin/main" className="right_side_small">뒤로가기</Link>
            <div className="formContent">
-               <div className={"taskStatistic"}>
-                   <div className={"wrapper_title"}>1. 음식점</div>
-                   <div className={"lightgray_wrapper"}>
-                       <div className={"submitFiles"}>제출한 파일 수 : {rows.length}개</div>
-                       <div className={"passFiles"}>Pass된 파일 수 : 0개</div>
-                       <Paper className={classes.root} style={{display: 'inline-block'}}>
-                          <TableContainer className={classes.container}>
-                            <Table stickyHeader aria-label="sticky table">
-                              <TableHead>
-                                <TableRow>
-                                  {columns.map((column) => (
-                                    <TableCell
-                                      key={column.id}
-                                      align={'center'}
-                                      style={{ minWidth: column.minWidth, backgroundColor: 'white', fontSize: '16px', fontWeight: 'bold',  }}
-                                    >
-                                      {column.label}
-                                    </TableCell>
-                                  ))}
-                                </TableRow>
-                              </TableHead>
-                              <TableBody>
-                                {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                                  return (
-                                    <TableRow hover role="checkbox" tabIndex={-1} key={row.time}>
-                                      {columns.map((column) => {
-                                        const value = row[column.id];
-                                        if (column.id == "fileName"){
-                                           return (
-                                              <TableCell key={column.id} align='center'
-                                                  style={{fontSize: '14px', fontWeight: 'normal', color:'black' }}>
-                                                    <Link to ="/admin/filedetail">
-                                                        {column.format && typeof value === 'number' ? column.format(value) : value}
-                                                    </Link>
-                                              </TableCell>
-                                            );
-                                        }else{
-                                            return (
-                                              <TableCell key={column.id} align='center'
-                                              style={{fontSize: '14px', fontWeight: 'normal' }}>
-                                                {column.format && typeof value === 'number' ? column.format(value) : value}
-                                              </TableCell>
-                                            );
-                                        }
-                                      })}
+               {submitter.Tasks.map((item)=>{
+                   let rows = item.Files;
+                   return(
+                     <div className={"taskStatistic"}>
+                       <div className={"wrapper_title"}>{item.TaskName}</div>
+                       <div className={"lightgray_wrapper"}>
+                           <div className={"submitFiles"}>제출한 파일 수 : {item.Total}개</div>
+                           <div className={"passFiles"}>Pass된 파일 수 : {item.Pass}개</div>
+                           <Paper className={classes.root} style={{display: 'inline-block'}}>
+                              <TableContainer className={classes.container}>
+                                <Table stickyHeader aria-label="sticky table">
+                                  <TableHead>
+                                    <TableRow>
+                                      {columns.map((column) => (
+                                        <TableCell
+                                          key={column.id}
+                                          align={'center'}
+                                          style={{ minWidth: column.minWidth, backgroundColor: 'white', fontSize: '16px', fontWeight: 'bold',  }}
+                                        >
+                                          {column.label}
+                                        </TableCell>
+                                      ))}
                                     </TableRow>
-                                  );
-                                })}
-                              </TableBody>
-                            </Table>
-                          </TableContainer>
-                          <TablePagination
-                            rowsPerPageOptions={[10, 20, 30]}
-                            component="div"
-                            count={rows.length}
-                            rowsPerPage={rowsPerPage}
-                            page={page}
-                            onChangePage={handleChangePage}
-                            onChangeRowsPerPage={handleChangeRowsPerPage}
-                          />
-                        </Paper>
-                   </div>
-               </div>
+                                  </TableHead>
+                                  <TableBody>
+                                    {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                                      return (
+                                        <TableRow hover role="checkbox" tabIndex={-1} key={row.FileName}>
+                                          {columns.map((column) => {
+                                            const value = row[column.id];
+                                            if (column.id == "FileName"){
+                                               return (
+                                                  <TableCell key={column.id} align='center'
+                                                      style={{fontSize: '14px', fontWeight: 'normal', color:'black' }}>
+                                                        <Link to ="/admin/filedetail">
+                                                            {column.format && typeof value === 'number' ? column.format(value) : value}
+                                                        </Link>
+                                                  </TableCell>
+                                                );
+                                            }else{
+                                                return (
+                                                  <TableCell key={column.id} align='center'
+                                                  style={{fontSize: '14px', fontWeight: 'normal' }}>
+                                                    {column.format && typeof value === 'number' ? column.format(value) : value}
+                                                  </TableCell>
+                                                );
+                                            }
+                                          })}
+                                        </TableRow>
+                                      );
+                                    })}
+                                  </TableBody>
+                                </Table>
+                              </TableContainer>
+                              <TablePagination
+                                rowsPerPageOptions={[10, 20, 30]}
+                                component="div"
+                                count={rows.length}
+                                rowsPerPage={rowsPerPage}
+                                page={page}
+                                onChangePage={handleChangePage}
+                                onChangeRowsPerPage={handleChangeRowsPerPage}
+                              />
+                            </Paper>
+                       </div>
+                   </div> );
+               })}
+
            </div>
        </div>
        </div>
