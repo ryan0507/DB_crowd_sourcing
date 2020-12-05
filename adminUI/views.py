0f44a80f19e80f93dd70cadff11656d6779daa83
2suh <leesuh_7@daum.net>
2020-12-05 15:30:59 +0900

    pass

--- adminUI/views.py
+++ adminUI/views.py
@@ -307,17 +307,20 @@ def UserUpdateView(request, infoID):
                 userID = "su " + data["Participant"][i]["UserID"]
                 tuple_arg = (userID, str(infoID))
                 sql = "UPDATE PARTICIPATE_TASK SET PASS = 'P' WHERE SubmitterID = %s AND TaskID = %s"
+                merge(dbconn, sql, tuple_arg)
+                dbconn.commit()
         for i in range(len(data["Request"])):
             if data["Request"][i]["Pass"] == "N":
                 userID = "su " + data["Request"][i]["UserID"]
                 tuple_arg = (userID, str(infoID))
                 sql = "DELETE FROM PARTICIPATE_TASK WHERE SubmitterID = %s AND TaskID = %s"
-            merge(dbconn, sql, tuple_arg)
-            dbconn.commit()
+                merge(dbconn, sql, tuple_arg)
+                dbconn.commit()
 
         return JsonResponse([], safe=False)
 
     except Exception as e:
+        print(e)
         return JsonResponse([], safe=False)
     finally:
         dbconn.close()
